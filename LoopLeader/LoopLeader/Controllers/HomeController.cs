193bf90662ef2421a7108db063818dfe854fc53b
Carl Skeel <carlskeel@yahoo.com>
2014-05-12 16:48:13 -0700

    Working on product stuff

--- LoopLeader/LoopLeader/Controllers/HomeController.cs
+++ LoopLeader/LoopLeader/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LoopLeader.Domain.Entities;
+using LoopLeader.Domain.Abstract;
+using LoopLeader.Domain.Concrete;
 
 namespace LoopLeader.Controllers
 {
@@ -15,6 +18,12 @@ namespace LoopLeader.Controllers
         {
             return View();
         }
+
+
+
+
+
+
 
     }
 }
